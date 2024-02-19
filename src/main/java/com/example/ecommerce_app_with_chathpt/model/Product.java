6efa2fff0f46b9a3d5a58d615b6b444960be878c
emreyurdagul <emreyurdagul@std.iyte.edu.tr>
2024-02-19 22:47:35 +0300

    Example conflict

---
+++ src/main/java/com/example/ecommerce_app_with_chathpt/model/Product.java
@@ -0,0 +1,35 @@
+package com.example.ecommerce_app_with_chathpt.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+@Data
+@Builder
+@Document(collection = "products")
+public class Product {
+
+    @Id
+    private String id;
+
+    @Field
+    private List<AttributeValue> attributes;
+    @Field
+    private Price price;
+    @Field
+    private boolean inStock;
+    @Field
+    private String url;
+    @Field
+    private String brand;
+    @Field
+    private String breadCrumbs;
+    @Field
+    private String title;
+    @Field
+    private String thumbnailImage;
+}
