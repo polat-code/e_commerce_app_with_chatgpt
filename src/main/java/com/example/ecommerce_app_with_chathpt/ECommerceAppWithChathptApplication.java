bee8e168723cb5ab400e0bbc0c35352d6b287ab9
Özgürhan Polat <ozgurhan.45@gmail.com>
2024-02-19 21:56:41 +0300

    First Commit

---
+++ src/main/java/com/example/ecommerce_app_with_chathpt/ECommerceAppWithChathptApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_app_with_chathpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAppWithChathptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceAppWithChathptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommerce_app_with_chathpt/ECommerceAppWithChathptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_app_with_chathpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppWithChathptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
